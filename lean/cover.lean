3e5db9ef26e40836749ca57812eaad22c707711a
Julian Berman <Julian@GrayVines.com>
2022-06-06 23:10:46 -0400

    Add a GitHub action to build LaTeX + a PDF on push (#32)
    * Vendor a copy of the Lean 4 pygments highlighter.
    
    This comes from the lean4 repo (recommended here for the forthcoming LaTeX
    setup):
    
        https://github.com/leanprover/lean4/blob/20e16f1c75b13057e00b4cc5315aac611df766fe/doc/syntax_highlight_in_latex.md#example-with-minted
    
    But besides making CI easier, it seems possible the book here will want to
    tweak things further so that its own examples are syntax highlighted (even if
    they aren't builtin syntax).
    
    * Repurpose cover.md for pandoc.
    
    (If there's use for it other than pandoc we can move this
    elsewhere, but it's needed for configuring the template).
    
    * Add a PDF link to the README.
    
    * Add a GitHub workflow

--- lean/cover.lean
+++ lean/cover.lean
@@ -1,13 +1,20 @@
 /-
-# Lean 4 Metaprogramming Book
+---
+title: "Metaprogramming in Lean 4"
+author: [Arthur Paulino, Damiano Testa, Edward Ayers, Henrik Böving,
+         Jannis Limperg, Siddhartha Gadgil, Siddharth Bhat]
+# abusing this field just because the template puts it at a decent right-sized spot
+date: "Formatted for PDF by Julian Berman using [Pascal Wagler's Template](https://github.com/Wandmalfarbe/pandoc-latex-template)"
+mainfont: "DejaVu Serif"
+monofont: "DejaVu Sans Mono"
+book: true
+header-right: "."
+titlepage: true
+titlepage-color: "083045"
+titlepage-text-color: "F4F6F7"
+titlepage-rule-color: "EDD369"
+keywords: [Lean, theorem proving, mathematics, math, maths, tutorial]
+...
 
-Authors in alphabetical order:
 
-* Arthur Paulino
-* Damiano Testa
-* Edward Ayers
-* Henrik Böving
-* Jannis Limperg
-* Siddhartha Gadgil
-* Siddharth Bhat
 -/
