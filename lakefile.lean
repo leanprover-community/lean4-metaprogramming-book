6e42b693dd2608912e9656c3773f57f9998eda0a
Evgenia Karunus <lakesare@gmail.com>
2023-01-05 12:40:31 +0400

    Add the "Solutions" section to lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -28,6 +28,7 @@ script build do
   if ← runCmd "python" #["-m", "lean2md", "lean", "md"] then return 1
   if ← runCmd "python" #["-m", "lean2md", "lean/main", "md/main"] then return 1
   if ← runCmd "python" #["-m", "lean2md", "lean/extra", "md/extra"] then return 1
+  if ← runCmd "python" #["-m", "lean2md", "lean/solutions", "md/solutions"] then return 1
 
   return 0
 
@@ -37,5 +38,6 @@ script viper_build do
   if ← runCmd "viper" #["-m", "lean2md", "lean", "md"] then return 1
   if ← runCmd "viper" #["-m", "lean2md", "lean/main", "md/main"] then return 1
   if ← runCmd "viper" #["-m", "lean2md", "lean/extra", "md/extra"] then return 1
+  if ← runCmd "viper" #["-m", "lean2md", "lean/solutions", "md/solutions"] then return 1
 
   return 0
