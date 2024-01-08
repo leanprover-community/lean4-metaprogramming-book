950c47db8eaa65bbf0166cde2aef615bc454039d
Seasawher <tellagoodstory@icloud.com>
2024-01-08 11:31:05 +0900

    install mdgen

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,9 @@ lean_lib «lean4-metaprogramming-book» where
   roots := #[`cover, `extra, `main, `solutions]
   globs := #[Glob.one `cover, Glob.submodules `extra, Glob.submodules `main, Glob.submodules `solutions]
 
+require mdgen from git
+  "https://github.com/Seasawher/mdgen" @ "main"
+
 def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
   let out ← IO.Process.output {
     cmd := cmd
