06d8d3fd44a7ad80835bb381f8aa5c206a7b93f8
Seasawher <tellagoodstory@icloud.com>
2024-04-20 10:41:46 +0900

    update Lean and mdgen
    * to use `#guard_msgs`
    * mdgen has to be updated

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ lean_lib «lean4-metaprogramming-book» where
   globs := #[Glob.one `cover, Glob.submodules `extra, Glob.submodules `main, Glob.submodules `solutions]
 
 require mdgen from git
-  "https://github.com/Seasawher/mdgen" @ "c501f6b8fa2502362bf94678d404857e46e2b65d"
+  "https://github.com/Seasawher/mdgen" @ "v1.3.0"
 
 def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
   let out ← IO.Process.output {
