ae3538e7da871b6c56a1f492ea19df7d60a0d564
JadAbouHawili <81527369+JadAbouHawili@users.noreply.github.com>
2026-07-13 11:30:06 +0300

    restore exercise order

--- lean/main/03_expressions.lean
+++ lean/main/03_expressions.lean
@@ -337,8 +337,8 @@ Tip: Use the trick just mentioned to check your answers!
 1. Create expression `1 + 2` with `Expr.app`.
 2. Create expression `1 + 2` with `Lean.mkAppN`.
 3. Create expression `fun x => 1 + x`.
-4. [**De Bruijn Indexes**] Create expression `fun x => fun y => x + y`.
-5. Create expression `fun a b c => (b * a) + c`.
+4. [**De Bruijn Indexes**] Create expression `fun a => fun b => fun c => (b * a) + c`.
+5. Create expression `fun x y => x + y`.
 6. Create expression `fun x, String.append "hello, " x`. Hint: use `Literal.strVal : String → Literal`.
 7. Create expression `∀ x : Prop, x ∧ x`. Hint: `Prop` is `Expr.sort Level.zero : Expr`
 8. Create expression `Nat → String`.
